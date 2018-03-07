ded319e4f385f8386c65df44e1bc17f675b6f1e8
mp5317 <mp5317@ic.ac.uk>
2018-03-07 04:50:52 +0000

    bugfix in responsivness

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -46,7 +46,7 @@ $(document).ready(function(){
         // Only prevent default if animation is actually gonna happen
         event.preventDefault();
         $('html, body').animate({
-          scrollTop: target.offset().top
+          scrollTop: target.offset().top-$('#topnavbar').outerHeight() - 10
         }, 1000, function() {
           // Callback after animation
           // Must change focus!
